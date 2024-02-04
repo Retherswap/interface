39344f5e1e46965e015c3d2785a4d97c8a531910
Retherswap Labs <knut-a_91@hotmail.com>
2024-02-04 12:57:48 +0100

    Update index.tsx
    Fixed Hypra Network discord link.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -112,7 +112,7 @@ export default function Menu() {
             <Code size={14} />
             Code
           </MenuItem>
-          <MenuItem id="link" href="https://discord.gg/rethereum-1131328579947462676">
+          <MenuItem id="link" href="https://discord.gg/xCB4AJDEFb">
             <MessageCircle size={14} />
             Discord
           </MenuItem>
